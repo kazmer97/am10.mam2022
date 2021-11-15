f86dbc98d094b55fe0eaec6a51212f3ad03e0485
Kazmer Nagy-Betegh <knagybetegh@yahoo.com>
2021-11-15 22:29:43 +0000

    assignment 2 backup

--- session2/session2_challenges.R
+++ session2/session2_challenges.R
@@ -4,7 +4,7 @@ library(here)
 library(extrafont)
 library(ggtext)
 
-loadfonts(device="win")
+loadfonts(device="pdf")
 
 # you must try to recreate
 # 1. session2_challenge.png (a static image where you have to use a shapefile and country results)
@@ -23,19 +23,50 @@ party_colours <- c("#2E74C0", "#CB454A")
 # remotes::install_github("UrbanInstitute/urbnmapr")
 library(urbnmapr)
 
+# set_urbn_defaults(style = "map")
+
 counties_sf <- get_urbn_map("counties", sf = TRUE)
 
+
+
 class(counties_sf)
-counties_sf$geometry
+# counties_sf <- st_crs(counties_sf, crs = "3857")
+
+st_crs(counties_sf)
 
 # having loaded the shapefile, let me print the map, 
 # where fill = grey70, and colour (the outline of each state) = white
 
 counties_sf %>% 
-  ggplot(aes()) +
+  ggplot(aes(geometry = geometry)) +
   geom_sf(fill = "grey70", colour = "#ffffff")
 
 # get latest NYT county-level data for the US 2020 election from this GitHub repository.
 # https://github.com/favstats/USElection2020-NYT-Results/tree/master/data/2020-11-10%2014-35-07
 
 results_president <- vroom::vroom(here("data", "results_president.csv"))
+
+# results_president <- left_join(results_president, counties_sf, by = c("fips"="county_fips"))
+
+map_margin <- results_president%>%
+  select(fips, margin2020)%>%
+  left_join(counties_sf, by = c("fips"="county_fips"), y.all=TRUE)
+  
+map_margin = st_as_sf(map_margin)
+
+map_margin %>% 
+  ggplot()+
+  geom_sf(aes(geometry = geometry, fill = margin2020))+
+  scale_fill_gradientn(#low = party_colours[1],
+                      #high = party_colours[2],
+                      colors = my_scale,
+                      na.value = "grey50",
+                      
+                      )+
+  # theme_light()+
+  theme(axis.line = element_blank(),
+        plot.background = element_blank(),
+        plot.margin = element_blank())
+  
+  
+
